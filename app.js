38559f9e8d61343947ae5ae41506fe33cf244a8a
SergikEnergy <kis8883244@gmail.com>
2022-10-31 17:00:41 +0300

    refactoring: 3.2 change names with the sense

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ var createNewTaskElement = function (taskString) {
   editInput.className = "task_input";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "button__change";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "button__delete";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -76,7 +76,7 @@ var editTask = function () {
 
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
+  var editBtn = listItem.querySelector(".button__change");
   var containsClass = listItem.classList.contains("edit__item");
   //If class of the parent is .editmode
   if (containsClass) {
@@ -138,8 +138,8 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var editButton = taskListItem.querySelector("button.button__change");
+  var deleteButton = taskListItem.querySelector("button.button__delete");
 
   //Bind editTask to edit button.
   editButton.onclick = editTask;
